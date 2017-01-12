6163f9aaa8dff0c11527095096c79de9484bdbd1
Alyona <b1ack_rain@mail.ru>
2017-01-12 14:40:13 +0200

    Added Feeds component

---
+++ scripts/Feeds.js
@@ -0,0 +1,30 @@
+(function () {
+    window.RSSChannelsApp = window.RSSChannelsApp || {};
+
+    function Feed() {
+        this.feednami = window.feednami;
+    }
+
+    Feed.prototype = {
+
+        loadFeed: function (url) {
+            this.feednami.load(url, function (result) {
+                if (result.error) {
+                    console.log(result.error);
+                    return result;
+
+                } else {
+                    var entries = result.feed.entries;
+                    for (var i = 0; i < entries.length; i++) {
+                        var entry = entries[i];
+                        console.log(entry.title);
+                        console.log(entry);
+                    }
+                    return entries;
+                }
+            })
+        }
+    };
+
+    window.RSSChannelsApp.Feednami = new Feed();
+})();
